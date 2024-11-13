787e6b3c82e369be86642dc678352fd37cc4f2f2
semenov <semenov>
2024-11-13 21:43:10 +0400

    1  компонент commandList

---
+++ src/component/commandList/commandListGeneratorHtml.ts
@@ -0,0 +1,11 @@
+export interface CommandListApi {
+    name: string;
+    action: string;
+    params: any;
+    result: string;
+    comment: string;
+}
+
+export function commandListGeneratorHtml() {
+    
+}
